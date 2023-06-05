bda30c1e97537e6488931f176fdbddf0dee30f87
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-06-05 22:02:30 +0400

    Create head, main and footer template

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header title="CV Application" />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
