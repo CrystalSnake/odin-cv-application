e5b689503658a02c94cda8f59be41470d82829c5
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2023-07-01 12:03:07 +0400

    Add tailwindcss to project

--- cv-project/src/components/Header.js
+++ cv-project/src/components/Header.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
+      <div className="bg-gray-100 py-4">
+        <h1 className="text-4xl font-bold text-green-500">
+          {this.props.title}
+        </h1>
       </div>
     );
   }
